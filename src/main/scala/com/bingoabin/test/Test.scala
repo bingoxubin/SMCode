ddbdf96b55c78ade0a07a3d16f1bb6b91d611579
bingoabin <757661238@qq.com>
2022-04-19 23:05:05 +0800

    spark坑

--- src/main/scala/com/bingoabin/test/Test.scala
+++ src/main/scala/com/bingoabin/test/Test.scala
@@ -1,7 +1,20 @@
 package com.bingoabin.test
 
+import org.apache.spark.{SparkConf, SparkContext}
+
+//坑
 object Test {
 	def main(args: Array[String]): Unit = {
+		val conf = new SparkConf().setAppName("local").setMaster("local")
+		val sc = new SparkContext(conf)
+		val rdd = sc.parallelize(Array(1, 2, 3, 4, 5))
 
+		var counter = 0
+
+		rdd.foreach {
+			n => counter += n
+		}
+		println(counter)
+		println(rdd.sum())
 	}
 }
