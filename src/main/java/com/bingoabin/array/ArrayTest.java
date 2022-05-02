86c2bcbc10bf8c2bd429015640839d6af901ce05
bingoabin <757661238@qq.com>
2022-05-02 11:31:04 +0800

    太平洋大西洋水流问题

---
+++ src/main/java/com/bingoabin/array/ArrayTest.java
@@ -0,0 +1,31 @@
+package com.bingoabin.array;
+
+import java.util.Arrays;
+
+/**
+ * @author bingoabin
+ * @date 2022/5/2 11:25
+ */
+public class ArrayTest {
+	public static void main(String[] args){
+		String str = "abbca";
+		System.out.println(appealSum(str));
+	}
+
+	public static long appealSum(String s) {
+		int n = s.length();
+		char[] chs = s.toCharArray();
+		int[] indexOfLastSame = new int[n], lastAppear = new int[26];
+		Arrays.fill(lastAppear, -1);
+		for (int i = 0; i < n; i++) {
+			indexOfLastSame[i] = lastAppear[chs[i] - 'a'];
+			lastAppear[chs[i] - 'a'] = i;
+		}
+		long pre = 1L, res = 1L;
+		for (int i = 1; i < n; i++) {
+			pre += i - indexOfLastSame[i];
+			res += pre;
+		}
+		return res;
+	}
+}
