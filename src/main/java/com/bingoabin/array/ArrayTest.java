89585488ed03f265ced48be6a0580c65c327b8ff
bingo abin <757661238@qq.com>
2023-02-12 19:32:27 +0800

    quicksort

--- src/main/java/com/bingoabin/array/ArrayTest.java
+++ src/main/java/com/bingoabin/array/ArrayTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  */
 public class ArrayTest {
 	public static void main(String[] args){
-		String str = "abbca";
+		String str = "abbcac";
 		System.out.println(appealSum(str));
 	}
 
